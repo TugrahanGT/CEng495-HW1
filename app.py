59e2f01bda20bd587c45152eaf08dd2cc6941806
Tuğrahan <tugrahan.gorkem@gmail.com>
2023-04-07 03:45:11 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from pymongo import MongoClient
+from bson.json_util import dumps
 
 app = Flask(__name__)
+client = MongoClient("mongodb+srv://tugrahangt:ZwW11yGKKlPJPJEE@clusterdb.9sbkfgg.mongodb.net/?retryWrites=true&w=majority")
+db = client.hw1_mongoDB
+
+users = db.users
+categories = db.categories
+
+categoriesList = list(categories.find())
+categoryNames = list()
+for category in categoriesList:
+    categoryNames.append(category["name"])
+print(categoryNames)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, World! " + str(category[0])
