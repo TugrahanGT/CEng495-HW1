b062ecb424f9864275de0f88fa9036b0fd5d8c43
TUGRAHAN GORKEM TURKMEN <e2337038@ceng.metu.edu.tr>
2023-04-08 05:01:16 +0300

    LogIn Page Trial!

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def index():
     return render_template("index.html", clothings = clothingProducts, computerComponents = computerComponentProducts,
                            monitors = monitorProducts, snacks = snackProducts)
 
-@app.route("/login/", methods = ("GET", "POST"))
+@app.route("/login", methods = ("GET", "POST"))
 def login():
     if request.method == "POST":
         username = request.form["username"]
