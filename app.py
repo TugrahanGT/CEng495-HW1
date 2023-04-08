f2148f2cec432a8c85021f7f049c3fd02b4c0fe3
TUGRAHAN GORKEM TURKMEN <e2337038@ceng.metu.edu.tr>
2023-04-08 04:26:36 +0300

    LogIn Page Trial!

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 from pymongo import MongoClient
 from bson.json_util import dumps
+import os
+
+secret_key = os.urandom(24).hex()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = secret_key
 client = MongoClient("mongodb+srv://tugrahangt:ZwW11yGKKlPJPJEE@clusterdb.9sbkfgg.mongodb.net/?retryWrites=true&w=majority")
 db = client.hw1_mongoDB
 
 users = db.users
+
+print(list(users.find({"username": "asd"})))
+
 categories = db.categories
 categoriesList = list(categories.find())
 
 clothings = categoriesList[0]
-computerComponents = categoriesList[0]
-monitors = categoriesList[0]
-snacks = categoriesList[0]
+computerComponents = categoriesList[1]
+monitors = categoriesList[2]
+snacks = categoriesList[3]
 
 clothingProducts = []
 computerComponentProducts = []
@@ -32,10 +39,28 @@ for item in monitors["items"]:
 for item in snacks["items"]:
     snackProducts.append(item)
 
-print(clothingProducts[0])
 
 @app.route("/")
 def index():
     print(clothingProducts)
     return render_template("index.html", clothings = clothingProducts, computerComponents = computerComponentProducts,
                            monitors = monitorProducts, snacks = snackProducts)
+
+@app.route("/login/", methods = ("GET", "POST"))
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if not username:
+            flash("Please enter a username!")
+        elif not password:
+            flash("Please enter a password!")
+        else:
+            result = list(users.find({"username": username}))
+            if not result:
+                flash("Wrong password or username")
+            elif password != result["password"]:
+                flash("Wrong password or username")
+            else:
+                return redirect(url_for("index"))
+    return render_template("login.html")
