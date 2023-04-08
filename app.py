f26baaa39e776c7a5c716639c0afca3f8f8e3658
TUGRAHAN GORKEM TURKMEN <e2337038@ceng.metu.edu.tr>
2023-04-08 09:21:13 +0300

    Rate

--- app.py
+++ app.py
@@ -87,35 +87,64 @@ def product():
     global user_loggedIn, clothingProducts, computerComponentProducts, snackProducts, monitorProducts
     if request.method == "POST":
         username = request.form["username"]
-        reviewText = request.form["review"]
         productID = int(request.form["itemID"])
         categoryID = int(request.form["categoryID"])
-        if add_review(username, reviewText, productID, categoryID):
-            categoriesList = list(categories.find())
-            if categoryID == 0:
-                clothingProducts = []
-                clothings = categoriesList[0]
-                for item in clothings["items"]:
-                    clothingProducts.append(item)
-                return render_template("product.html", user = user_loggedIn, item = clothingProducts[productID], categoryID = categoryID)
-            elif categoryID == 1:
-                computerComponentProducts = []
-                computerComponents = categoriesList[1]
-                for item in computerComponents["items"]:
-                    computerComponentProducts.append(item)
-                return render_template("product.html", user = user_loggedIn, item = computerComponentProducts[productID], categoryID = categoryID)
-            elif categoryID == 2:
-                monitorProducts = []
-                monitors = categoriesList[2]
-                for item in monitors["items"]:
-                    monitorProducts.append(item)
-                return render_template("product.html", user = user_loggedIn, item = monitorProducts[productID], categoryID = categoryID)
-            else:
-                snackProducts = []
-                snacks = categoriesList[3]
-                for item in snacks["items"]:
-                    snackProducts.append(item)
-                return render_template("product.html", user = user_loggedIn, item = snackProducts[productID], categoryID = categoryID)
+        if request.form["submit_btn"] == "rev":
+            reviewText = request.form["review"]
+            if add_review(username, reviewText, productID, categoryID):
+                categoriesList = list(categories.find())
+                if categoryID == 0:
+                    clothingProducts = []
+                    clothings = categoriesList[0]
+                    for item in clothings["items"]:
+                        clothingProducts.append(item)
+                    return render_template("product.html", user = user_loggedIn, item = clothingProducts[productID], categoryID = categoryID)
+                elif categoryID == 1:
+                    computerComponentProducts = []
+                    computerComponents = categoriesList[1]
+                    for item in computerComponents["items"]:
+                        computerComponentProducts.append(item)
+                    return render_template("product.html", user = user_loggedIn, item = computerComponentProducts[productID], categoryID = categoryID)
+                elif categoryID == 2:
+                    monitorProducts = []
+                    monitors = categoriesList[2]
+                    for item in monitors["items"]:
+                        monitorProducts.append(item)
+                    return render_template("product.html", user = user_loggedIn, item = monitorProducts[productID], categoryID = categoryID)
+                else:
+                    snackProducts = []
+                    snacks = categoriesList[3]
+                    for item in snacks["items"]:
+                        snackProducts.append(item)
+                    return render_template("product.html", user = user_loggedIn, item = snackProducts[productID], categoryID = categoryID)
+            elif request.form["submit_btn"] == "rate":
+                rating = int(request.form["rating"])
+                if add_rating(username, rating, productID, categoryID):
+                    categoriesList = list(categories.find())
+                    if categoryID == 0:
+                        clothingProducts = []
+                        clothings = categoriesList[0]
+                        for item in clothings["items"]:
+                            clothingProducts.append(item)
+                        return render_template("product.html", user = user_loggedIn, item = clothingProducts[productID], categoryID = categoryID)
+                    elif categoryID == 1:
+                        computerComponentProducts = []
+                        computerComponents = categoriesList[1]
+                        for item in computerComponents["items"]:
+                            computerComponentProducts.append(item)
+                        return render_template("product.html", user = user_loggedIn, item = computerComponentProducts[productID], categoryID = categoryID)
+                    elif categoryID == 2:
+                        monitorProducts = []
+                        monitors = categoriesList[2]
+                        for item in monitors["items"]:
+                            monitorProducts.append(item)
+                        return render_template("product.html", user = user_loggedIn, item = monitorProducts[productID], categoryID = categoryID)
+                    else:
+                        snackProducts = []
+                        snacks = categoriesList[3]
+                        for item in snacks["items"]:
+                            snackProducts.append(item)
+                        return render_template("product.html", user = user_loggedIn, item = snackProducts[productID], categoryID = categoryID)
     itemID = int(request.args.get("product"))
     categoryID = int(request.args.get("categoryID"))
     if categoryID == 0:
@@ -289,6 +318,211 @@ def add_rev_snack(username, reviewText, productID):
             {
                 "$push": {
                     "items.$.reviews": newReview
+                }
+            }
+        )
+    return True
+
+
+def add_rating(username, rating, productID, categoryID):
+    global clothingProducts, computerComponentProducts, snackProducts, monitorProducts, categories
+    if categoryID == 0:
+        return add_rate_clothing(username, rating, productID)
+    elif categoryID == 1:
+        return add_rate_compCom(username, rating, productID)
+    elif categoryID == 2:
+        return add_rate_mon(username, rating, productID)
+    else:
+        return add_rate_snack(username, rating, productID)
+    
+
+def add_rate_clothing(username, rating, productID):
+    global clothingProducts, computerComponentProducts, snackProducts, monitorProducts, categories
+    ratingList = clothingProducts[productID]["ratings"]
+    idx, flag, totalRating = 0, False, 0
+    for rating in ratingList:
+        if rating["author"] == username:
+            flag = True
+            break
+        idx += 1
+    if flag:
+        ratingList[idx]["rating"] = rating
+        ratingCount = len(ratingList)
+        for rating in ratingList:
+            totalRating += rating["rating"]
+        avgRating = totalRating / ratingCount
+        categories.update_one(
+            {
+                "_id": 0,
+                "items.itemID": productID
+            },
+            {
+                "$set": {
+                    "items.$.ratings": ratingList,
+                    "items.$.rating": avgRating
+                }
+            }
+        )
+    else:
+        lastRatingID = ratingList[len(ratingList) - 1]["ratingID"]
+        ratingCount = len(ratingList)
+        for rating in ratingList:
+            totalRating += rating["rating"]
+        avgRating = (totalRating + rating) / (ratingCount + 1)
+        newRating = {"ratingID": lastRatingID + 1, "rating": rating, "author": username}
+        categories.update_one(
+            {
+                "_id": 0,
+                "items.itemID": productID
+            },
+            {
+                "$push": {
+                    "items.$.ratings": newRating,
+                    "items.$.rating": avgRating
+                }
+            }
+        )
+    return True
+
+def add_rate_compCom(username, rating, productID):
+    global clothingProducts, computerComponentProducts, snackProducts, monitorProducts, categories
+    ratingList = computerComponentProducts[productID]["ratings"]
+    idx, flag, totalRating = 0, False, 0
+    for rating in ratingList:
+        if rating["author"] == username:
+            flag = True
+            break
+        idx += 1
+    if flag:
+        ratingList[idx]["rating"] = rating
+        ratingCount = len(ratingList)
+        for rating in ratingList:
+            totalRating += rating["rating"]
+        avgRating = totalRating / ratingCount
+        categories.update_one(
+            {
+                "_id": 1,
+                "items.itemID": productID
+            },
+            {
+                "$set": {
+                    "items.$.ratings": ratingList,
+                    "items.$.rating": avgRating
+                }
+            }
+        )
+    else:
+        lastRatingID = ratingList[len(ratingList) - 1]["ratingID"]
+        ratingCount = len(ratingList)
+        for rating in ratingList:
+            totalRating += rating["rating"]
+        avgRating = (totalRating + rating) / (ratingCount + 1)
+        newRating = {"ratingID": lastRatingID + 1, "rating": rating, "author": username}
+        categories.update_one(
+            {
+                "_id": 1,
+                "items.itemID": productID
+            },
+            {
+                "$push": {
+                    "items.$.ratings": newRating,
+                    "items.$.rating": avgRating
+                }
+            }
+        )
+    return True
+
+def add_rate_mon(username, rating, productID):
+    global clothingProducts, computerComponentProducts, snackProducts, monitorProducts, categories
+    ratingList = monitorProducts[productID]["ratings"]
+    idx, flag, totalRating = 0, False, 0
+    for rating in ratingList:
+        if rating["author"] == username:
+            flag = True
+            break
+        idx += 1
+    if flag:
+        ratingList[idx]["rating"] = rating
+        ratingCount = len(ratingList)
+        for rating in ratingList:
+            totalRating += rating["rating"]
+        avgRating = totalRating / ratingCount
+        categories.update_one(
+            {
+                "_id": 2,
+                "items.itemID": productID
+            },
+            {
+                "$set": {
+                    "items.$.ratings": ratingList,
+                    "items.$.rating": avgRating
+                }
+            }
+        )
+    else:
+        lastRatingID = ratingList[len(ratingList) - 1]["ratingID"]
+        ratingCount = len(ratingList)
+        for rating in ratingList:
+            totalRating += rating["rating"]
+        avgRating = (totalRating + rating) / (ratingCount + 1)
+        newRating = {"ratingID": lastRatingID + 1, "rating": rating, "author": username}
+        categories.update_one(
+            {
+                "_id": 2,
+                "items.itemID": productID
+            },
+            {
+                "$push": {
+                    "items.$.ratings": newRating,
+                    "items.$.rating": avgRating
+                }
+            }
+        )
+    return True
+
+def add_rate_snack(username, rating, productID):
+    global clothingProducts, computerComponentProducts, snackProducts, monitorProducts, categories
+    ratingList = snackProducts[productID]["ratings"]
+    idx, flag, totalRating = 0, False, 0
+    for rating in ratingList:
+        if rating["author"] == username:
+            flag = True
+            break
+        idx += 1
+    if flag:
+        ratingList[idx]["rating"] = rating
+        ratingCount = len(ratingList)
+        for rating in ratingList:
+            totalRating += rating["rating"]
+        avgRating = totalRating / ratingCount
+        categories.update_one(
+            {
+                "_id": 3,
+                "items.itemID": productID
+            },
+            {
+                "$set": {
+                    "items.$.ratings": ratingList,
+                    "items.$.rating": avgRating
+                }
+            }
+        )
+    else:
+        lastRatingID = ratingList[len(ratingList) - 1]["ratingID"]
+        ratingCount = len(ratingList)
+        for rating in ratingList:
+            totalRating += rating["rating"]
+        avgRating = (totalRating + rating) / (ratingCount + 1)
+        newRating = {"ratingID": lastRatingID + 1, "rating": rating, "author": username}
+        categories.update_one(
+            {
+                "_id": 3,
+                "items.itemID": productID
+            },
+            {
+                "$push": {
+                    "items.$.ratings": newRating,
+                    "items.$.rating": avgRating
                 }
             }
         )
